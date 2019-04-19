aeb70fd25e45df8d055572fd96c939c3e3e10e79
Peeyush <42058825+peeyushster@users.noreply.github.com>
2019-04-18 22:26:00 -0400

    Update displayData.m

--- Octave/displayData.m
+++ Octave/displayData.m
@@ -1,4 +1,5 @@
 function [h, display_array] = displayData(X, example_width)
+
 %DISPLAYDATA Display 2D data in a nice grid
 %   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
 %   stored in X in a nice grid. It returns the figure handle h and the 
